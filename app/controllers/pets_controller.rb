1a06619c955d4d8773af819278675c837ee76fab
Joana Palero <joana.palero24@gmail.com>
2018-08-25 21:04:15 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,41 +2,54 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     @owners = Owner.all
+    #@pets = Pet.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-    if (@pet = Pet.create(params[:pet]))
-      @pet.update(owner: @owner) if (@pet.owner.nil? && (@owner = Owner.create(params[:owner])))
-      redirect to "pets/#{@pet.id}"
-    else
-      erb :'/pets/index'
+  post '/pets' do
+    @pet = Pet.create(params[:pet])
+    #binding.pry
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
     end
+    @pet.save
+    redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
+    #binding.pry
     erb :'/pets/show'
   end
 
-  get '/pets/:id/edit' do 
-    if @pet = Pet.find_by(id: params[:id])
-      @owners = Owner.all
-      erb :'pets/edit'
-    else
-      redirect '/pets'
-    end
+  get '/pets/:id/edit' do
+    @pet = Pet.find(params[:id])
+    @owner = Owner.all
+    # binding.pry
+    erb :'/pets/edit'
   end
 
+  # it " loads form to edit a pet and his owner" do
+  #   visit "/pets/#{@pet.id}/edit"
+  #   expect(page).to have_field('pet_name')
+  #   expect(page.has_checked_field?(@owner.id)).to eq(true)
+  #   expect(page).to have_field('owner[name]')
+  # end
+
   post '/pets/:id' do
+    #binding.pry
     @pet = Pet.find(params[:id])
-    @pet.update(params[:pet])
-    @pet.update(owner: @owner) if ((@owner = Owner.new(params[:owner])) && @owner.save)
-    redirect "/pets/#{@pet.id}"
+    @pet.update(params["pet"])
+    #binding.pry
+    if !params["owner"]["name"].empty?
+      @pet.owner = Owner.create(name: params["owner"]["name"])
+    end
+    @pet.save
+    redirect to "pets/#{@pet.id}"
   end
 end
